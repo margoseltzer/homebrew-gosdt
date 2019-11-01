b1d2c51a48df5784e4071fab141adf44b97830f2
Jimmy Lin <jimmy.lin1313@gmail.com>
2019-11-01 15:14:52 -0700

    first release!

--- gosdt.rb
+++ gosdt.rb
@@ -2,16 +2,16 @@
 #                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Gosdt < Formula
-  desc "Generalized Optimal Sparse Decisio Trees."
-  homepage "https://github.com/margoseltzer/homebrew-gosdt"
-  url "https://github.com/margoseltzer/homebrew-gosdt/blob/master/gosdt.tar.gz"
+class GOSDT < Formula
   version "0.1"
-  sha256 "9eec103ed577189f99e34b957b00f01dc0c72bcf1d9b0f0c1d4eb466ab6627d4"
-  # depends_on "cmake" => :build
+  desc "Generalized Optimal Sparse Decisio Trees"
+  homepage "https://github.com/margoseltzer/homebrew-gosdt"
+
+  url "https://github.com/margoseltzer/homebrew-gosdt/blob/master/gosdt-0.1.0.tar.gz"
+  sha256 "4ce6982fce5cce2aa530c9f4d8db571a274ddab746e18e5546cec02f08a8c735"
+
   depends_on "boost"
   depends_on "tbb"
-  depends_on "python"
 
   def install
     system "./configure", "--disable-debug",
